e769cc0cb648eb0231aa7410f20f3b6251a6ee7b
Joao-Melgarejo <joaomelgarejoaddp@gmail.com>
2025-12-06 18:13:01 -0500

    activity class

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Rectangle s1 = new Rectangle("Brown", 44, 32);
+        shapes.Add(s1);
+        
+        Square s2 = new Square("Green", 52);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("White", 54);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"the {color} shape has an area of {area}.");
+        }
+
     }
 }
