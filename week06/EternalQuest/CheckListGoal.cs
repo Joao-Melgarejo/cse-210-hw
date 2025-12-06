f174e19e15f163d0e0a79ec6663d8b650ee9c2b5
Joao-Melgarejo <joaomelgarejoaddp@gmail.com>
2025-12-06 18:40:43 -0500

    final

---
+++ week06/EternalQuest/CheckListGoal.cs
@@ -0,0 +1,47 @@
+using System;
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, string points, int target, int bonus) : base(name, description, points)
+    {
+        _amountCompleted = 0;
+        _target = target;
+        _bonus = bonus;
+    }
+
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public override string GetDetailsString()
+    {
+        string status = IsComplete() ? "[X]" : "[ ]";
+        return $"{status} {GetName()} ({GetDescription()}) -- Currently completed: {_amountCompleted}/{_target}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal:{GetName()},{GetDescription()},{GetPoints()},{_bonus},{_target},{_amountCompleted}";
+    }
+    public int GetBonus()
+    {
+        return _bonus;
+    }
+    public int GetCurrentCount()
+    {
+        return _amountCompleted;
+    }
+    public int GetTarget()
+    {
+        return _target;
+    }
+}
